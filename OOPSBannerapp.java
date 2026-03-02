5d26bf31842019d95effbc45d7e937db6abf3589
Prometheus2171 <is2982@srmist.edu.in>
2026-03-02 09:29:19 +0530

    Update OOPSBannerapp.java

--- OOPSBannerapp.java
+++ OOPSBannerapp.java
@@ -1,5 +1,25 @@
-public class OOPSBannerapp{
- public static void main(String args[]){
-     System.out.println("OOPS");
+/**
+ * OOPSBannerApp UC4 – Render OOPS as Banner using String Array and Loop
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Store banner lines inside a String array
+        String[] banner = {
+            " ***     ***     ******     ***** ",
+            "**   **  **   **  **   **    **   **",
+            "**   **  **   **  **   **    **   **",
+            "**   **  **   **  ******     ***** ",
+            "**   **  **   **  **         **   **",
+            "**   **  **   **  **         **   **",
+            " ***     ***     **         ***** "
+        };
+
+        // Print using loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
