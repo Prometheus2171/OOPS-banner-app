0789f85962547bee3b8d0bac7d38b2c39d87c852
Prometheus2171 <is2982@srmist.edu.in>
2026-03-02 09:30:11 +0530

    Update OOPSBannerapp.java

--- OOPSBannerapp.java
+++ OOPSBannerapp.java
@@ -1,5 +1,28 @@
-public class OOPSBannerapp{
- public static void main(String args[]){
-     System.out.println("OOPS");
+/**
+ * OOPSBannerApp UC3 – OOPS Banner Application (Use Case 3)
+ * Using String.join() for better readability and efficiency
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " *** ",   " *** ",   " ****** ", " ***** "));
+
+        System.out.println(String.join(" ",
+                "**   **", "**   **", "**      ", "**   **"));
+
+        System.out.println(String.join(" ",
+                "**   **", "**   **", "******  ", "*****  "));
+
+        System.out.println(String.join(" ",
+                "**   **", "**   **", "**      ", "**      "));
+
+        System.out.println(String.join(" ",
+                "**   **", "**   **", "**      ", "**      "));
+
+        System.out.println(String.join(" ",
+                " *** ",   " *** ",   "**      ", " ***** "));
     }
 }
