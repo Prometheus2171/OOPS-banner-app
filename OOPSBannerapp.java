84c5cd11363f3cd3c7ce9555c96213b976a39ddf
Prometheus2171 <is2982@srmist.edu.in>
2026-03-02 09:42:25 +0530

    Update OOPSBannerapp.java

--- OOPSBannerapp.java
+++ OOPSBannerapp.java
@@ -1,5 +1,45 @@
-public class OOPSBannerapp{
- public static void main(String args[]){
-     System.out.println("OOPS");
+public class OOPSBannerApp {
+
+    // Main method to run the banner display
+    public static void main(String[] args) {
+
+        // Define String Array variable to hold the OOPS banner lines
+        // Each line represents a row in the banner for the letters O, O, P, S
+        String[] lines = {
+            String.join("   ",
+                "  OOO  ",
+                "  OOO  ",
+                "  PPPP ",
+                "  SSSS "),
+            
+            String.join("   ",
+                " O   O ",
+                " O   O ",
+                " P   P ",
+                " S     "),
+            
+            String.join("   ",
+                " O   O ",
+                " O   O ",
+                " PPPP  ",
+                "  SSS  "),
+            
+            String.join("   ",
+                " O   O ",
+                " O   O ",
+                " P     ",
+                "     S "),
+            
+            String.join("   ",
+                "  OOO  ",
+                "  OOO  ",
+                " P     ",
+                " SSSS  ")
+        };
+
+        // Use a loop to print each line of the banner
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
